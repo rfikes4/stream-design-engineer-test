58c165482aae566a7b76a150a249d192f906a596
rfikes4 <robbie@staratlas.com>
2024-12-03 17:53:25 -0700

    add control links and toggle expand

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -4,15 +4,19 @@ import './Controls.css';
 const Controls: React.FC = () => {
   return (
     <div className="controls">
-      <div className="control-icon">
-        <img src="./images/icon-left.svg" alt="Icon Left" />
-      </div>
-      <div className="control-icon-up" >
+      <a href="https://github.com/rfikes4/stream-design-engineer-test/tree/master" target="_blank" rel="noopener noreferrer">
+        <div className="control-icon">
+          <img src="./images/icon-left.svg" alt="Icon Left" />
+        </div>
+      </a>
+      <div className="control-icon-up">
         <img src="./images/icon-up.svg" alt="Icon Up" />
       </div>
-      <div className="control-icon">
-        <img src="./images/icon-right.svg" alt="Icon Right" />
-      </div>
+      <a href="https://www.robertfikesiv.com/" target="_blank" rel="noopener noreferrer">
+        <div className="control-icon">
+          <img src="./images/icon-right.svg" alt="Icon Right" />
+        </div>
+      </a>
     </div>
   );
 };
